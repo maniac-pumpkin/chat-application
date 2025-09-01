2839c977d1d6d918ee38231993d75921313b8c68
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-09-01 17:17:48 +0330

    fix: add tanstackRouter plugin to Vite configuration for improved routing support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import tailwindcss from "@tailwindcss/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      autoCodeSplitting: true,
+      target: "react",
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
