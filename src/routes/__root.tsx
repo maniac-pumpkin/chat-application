d149d63cbb2d241964ad5152980157c2ea1f05d3
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-09-20 17:50:41 +0330

    feat: wrap RootComponent with ThemeProvider for theme management

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,15 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 
+import { ThemeProvider } from "@/components/theme-provider"
+
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
   return (
-    <>
-      <div>Hello "__root"!</div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Outlet />
-    </>
+    </ThemeProvider>
   )
 }
