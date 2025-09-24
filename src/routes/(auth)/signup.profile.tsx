3134d9cc9106b3da6068c3c513678b704a23e230
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-09-24 23:38:11 +0330

    feat: add signup profile route with ClientAuthForm for user profile completion

---
+++ src/routes/(auth)/signup.profile.tsx
@@ -0,0 +1,41 @@
+import { createFileRoute } from "@tanstack/react-router"
+import { type FormEvent } from "react"
+
+import ClientAuthForm from "@/components/others/client-auth-form"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+
+export const Route = createFileRoute("/(auth)/signup/profile")({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    const data = new FormData(e.currentTarget)
+    const payload = {
+      fullname: String(data.get("fullname") || ""),
+      password: String(data.get("bio") || ""),
+    }
+
+    console.log(payload)
+  }
+
+  return (
+    <ClientAuthForm
+      footer={{ linkHref: "/signup", linkText: "Sign up page", text: "Go back to" }}
+      header={{ description: "Tell us more about yourself", title: "Complete your profile" }}
+      submitBtnText="Sign up"
+      submitFn={handleSubmit}
+    >
+      <Label className="block space-y-2">
+        <span className="block">Username</span>
+        <Input name="username" placeholder="ex: admin" required type="text" />
+      </Label>
+      <Label className="block space-y-2">
+        <span className="block">Bio</span>
+        <Textarea name="bio" placeholder="Your biography here..." required />
+      </Label>
+    </ClientAuthForm>
+  )
+}
